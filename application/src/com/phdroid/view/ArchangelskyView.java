c6ba704568b666b6d71404336cf4b38f3ed3f26b
Max Korenkov <mkorenkov@gmail.com>
2011-08-27 20:18:20 +0300

    added dependency on androidannotations

---
+++ application/src/com/phdroid/view/ArchangelskyView.java
@@ -0,0 +1,19 @@
+package com.phdroid.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ArchangelskyView extends View {
+	public ArchangelskyView(Context context) {
+		super(context);
+	}
+
+	public ArchangelskyView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public ArchangelskyView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+}
