9f1edecb7b76dd4fecb89ee9f3646da57fa5adfd
Max Korenkov <mkorenkov@gmail.com>
2011-08-29 00:42:31 +0300

    tasks annotations

--- application/src/com/phdroid/dao/Task.java
+++ application/src/com/phdroid/dao/Task.java
@@ -1,15 +1,25 @@
 package com.phdroid.dao;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 import java.util.Date;
 
 /**
  * Task class. Tasks can be assigned to specific day and their order is not important.
  */
+@DatabaseTable(tableName = "tasks")
 public class Task implements DaoObject {
+	@DatabaseField(id=true)
 	private int id;
+	@DatabaseField
 	private String summary;
+	@DatabaseField
 	private String description;
+	@DatabaseField
 	private Date when;
+	@DatabaseField
+	private int priority;
 
 	public int getId() {
 		return id;
